5e5db5bccccc58cf07b4226b77cf46086f062ec4
Juan Cárdenas Silva <juanpoodres@gmail.com>
2026-05-18 17:02:07 -0500

    Inicio del Proyecto

---
+++ Desktop/tasks-nextjs-supabase/components/ui/input.tsx
@@ -0,0 +1,50 @@
+"use client"
+
+import * as React from "react"
+import { cn } from "@/lib/utils"
+import { Eye, EyeOff } from "lucide-react"
+
+export interface InputProps
+  extends React.InputHTMLAttributes<HTMLInputElement> { }
+
+const Input = React.forwardRef<HTMLInputElement, InputProps>(
+  ({ className, type, ...props }, ref) => {
+    const [showPassword, setShowPassword] = React.useState(false)
+
+    const togglePasswordVisibility = () => {
+      setShowPassword(!showPassword)
+    }
+
+    // Determinar el tipo de input a mostrar
+    const inputType = type === "password" && showPassword ? "text" : type
+
+    return (
+      <div className="relative">
+        <input
+          type={inputType}
+          className={cn(
+            "flex h-10 bg-background w-full rounded-md border border-input px-3 py-2 text-sm ring-offset-background file:bor",
+            type === "password" ? "pr-10" : "",
+            className
+          )}
+          ref={ref}
+          {...props}
+        />
+        {type === "password" && (
+          <button
+            type="button"
+            className="absolute right-3 top-1/2 -translate-y-1/2 text-muted-foreground hover:text-foreground"
+            onClick={togglePasswordVisibility}
+            aria-label={showPassword ? "Ocultar contraseña" : "Mostrar contraseña"}
+          >
+            {showPassword ? <EyeOff /> : <Eye />}
+          </button>
+        )}
+      </div>
+    )
+  }
+)
+
+Input.displayName = "Input"
+
+export { Input }
